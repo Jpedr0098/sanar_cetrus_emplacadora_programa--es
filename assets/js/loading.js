33ec3f1dcd0c7c045b7dfc2ded354f1fec82c58f
Jpedr098 <joao.maurano.ismart@gmail.com>
2024-10-29 18:43:36 -0300

    ultimos detalhes pequenos

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const nomePredio = document.querySelector(".localCetrus h1");
     const enderecoPredio = document.querySelector(".localCetrus p");
 
-    nomePredio.textContent = p.predio || "Nome não disponível";
-    enderecoPredio.textContent = p.local || "Endereço não disponível";
+    nomePredio.textContent = p.predio
+    enderecoPredio.textContent = p.local
+
+    document.title = `Locação ${p.predio}`
 })
